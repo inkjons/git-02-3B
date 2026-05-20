b378a2796bc19130ae5bcda7e9f4cfab535101a7
jonathan <1nfinity70mega@gmail.com>
2026-05-20 13:51:41 -0600

    [NEW]clase-calculadora y constructor

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculadora {
+    private double numero1;
+    private double numero2;
+
+    public Calculadora(double numero1, double numero2){
+        this.numero1=numero1;
+    this.numero2=numero2;
+    }
+}
